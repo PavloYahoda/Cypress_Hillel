ea17b147588339b7f44bc45b71a1cf5db7ed68ff
Pavlo Yahoda <pavlo.yahoda@gmail.com>
2024-12-12 19:25:58 +0200

    + new config

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,12 @@ module.exports = defineConfig({
   e2e: {
     chromeWebSecurity: false,
     //baseUrl: '',
-    },   
+    retries: {
+      runMode: 2,
+      openMode: 3
+    },
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    video: true,
+  },
 });
